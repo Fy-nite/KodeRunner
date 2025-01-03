92dd339d49a8ba1f2d8f6b93281f4c3cebacc49a
charliesan <charliesan@envs.net>
2025-01-03 11:08:49 +1100

    non functioning

--- editor/main.py
+++ editor/main.py
@@ -8,16 +8,7 @@ from ui.welcomewindow import WelcomeWindow
 
 def main():
     app = QApplication(sys.argv) # start the welcome window
-    if os.path.exists("projects.db"):
-        pass
-    else:
-         # make the dB
 
-         conn = sqlite3.connect("projects.db")
-         cursor = conn.cursor()
-         cursor.execute("CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, path TEXT)")
-         conn.commit()
-         conn.close()
     window = WelcomeWindow()
     window.show()
     
